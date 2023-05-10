33ba123c4195f9bf8ca4c613e36ce29d7094fe67
khinsoenyein <khinsoenyein@gmail.com>
2023-05-10 11:49:23 +0630

    Upload Project Files

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const app = express();
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    connectionLimit: '10',
+});
+
+app.get('/', (req, res) => {
+    pool.query('SELECT * FROM test', (error, result) => {
+        if (error) {
+            console.error(error);
+            res.send(error);
+            return;
+        }
+
+        res.send(result);
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
